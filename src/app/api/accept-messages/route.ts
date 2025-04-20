3818257ee7e63fe4d2544d7c005631c5d5bc31f5
manas0603 <manassharma850@gmail.com>
2025-04-20 21:53:01 +0530

    New Change

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -78,14 +78,14 @@ export async function GET(request: Request) {
         return Response.json({
             success: false,
             message: "User not found",
-        },{status:401})
+        },{status:404})
         
     } 
     else{
         return Response.json({
             success: true,
-            message: "User found",
+            isAcceptingMessages: foundUser.isAcceptingMessages,
             user:foundUser
         },{status:200})
-    }
+    }  
 }
